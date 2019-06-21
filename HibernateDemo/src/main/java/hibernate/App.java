a8aabaec7fb02500d13013a7901086fb39e98100
WoronowiczGIT <woronowiczJava@gmail.com>
2019-06-21 18:33:35 +0200

    hibernate is shit :<<<

--- HibernateDemo/src/main/java/hibernate/App.java
+++ HibernateDemo/src/main/java/hibernate/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
+        try{
+
+
+
+        }catch(Throwable e){}
+
         System.out.println( "Hello World!" );
     }
 }
