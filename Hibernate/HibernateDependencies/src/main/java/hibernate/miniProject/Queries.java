f78722f839bf50a389a8dc77f2762058b697e0cd
WoronowiczGIT <woronowiczJava@gmail.com>
2019-06-23 15:55:28 +0200

    i suck at sql

--- Hibernate/HibernateDependencies/src/main/java/hibernate/miniProject/Queries.java
+++ Hibernate/HibernateDependencies/src/main/java/hibernate/miniProject/Queries.java
@@ -2,10 +2,14 @@ package hibernate.miniProject;
 
 import hibernate.HibernateUtils;
 import hibernate.miniProject.models.*;
+import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
 
 import javax.persistence.EntityManager;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class Queries {
     final static SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
@@ -15,18 +19,98 @@ public class Queries {
 
 //        generateTaskless();
 //        removeTaskless();
-        extractBlockerAndChangeName();
+//        extractBlockerAndChangeName();
 //        printByPhoneBrand("IPHONE");
-
-
+//            printTaskPage(4);
+        fromMostToLeast();
         HibernateUtils.shutdown();
     }
+    public static void fromMostToLeast(){
+        manager.getTransaction().begin();
+        String s = "SELECT e FROM Employees e Join fetch e.tasksSet";
 
+
+
+        List<Employees> taskMaster = manager.createQuery(s,Employees.class).getResultList();
+
+        manager.getTransaction().commit();
+    }
+    // NOT COMPLETE
+    public static void transferTasks(){
+        manager.getTransaction().begin();
+        List<Employees> employees = manager.createQuery(
+                "select e from Employees",Employees.class)
+                .getResultList();
+
+        int totaltasks = 0;
+
+        for (Employees e: employees
+             ) {
+            totaltasks += e.getTasksSet().size();
+        }
+        int taskAvg = totaltasks / employees.size();
+
+        Set<Tasks> spareTasks = new HashSet<>();
+
+        for (Employees e : employees
+             ) {
+            Iterator<Tasks> iterator = e.getTasksSet().iterator();
+            while(e.getTasksSet().size() > taskAvg && iterator.hasNext()){
+                spareTasks.add(iterator.next());
+                iterator.remove();
+            }
+        }
+        Iterator<Tasks> tasksIterator = spareTasks.iterator();
+        Iterator<Employees> employeesIterator = employees.iterator();
+        while (tasksIterator.hasNext()){
+            employeesIterator.next().getTasksSet().add(tasksIterator.next());
+        }
+
+        manager.getTransaction().commit();
+    }
+
+
+    public static void printTaskPage(int pageNumber){
+        if(pageNumber <= 1) pageNumber = 0;else pageNumber = (pageNumber-1)*3;
+        manager.getTransaction().begin();
+
+        List<Tasks> tasks = manager.createQuery("select t from Tasks t",Tasks.class)
+                .setFirstResult(pageNumber)
+                .setMaxResults(pageNumber+3)
+                .getResultList();
+
+        manager.getTransaction().commit();
+
+        for (Tasks t: tasks
+             ) {
+            System.out.println(t.getTaskID()+" "+t.getName()+" "+t.getPriority());
+        }
+    }
+
+    public static void generateTasks(int employeeID){
+        addTasksToUser(employeeID,"newTask",Priority.HIGH_PRIORITY);
+        addTasksToUser(employeeID,"newTask2",Priority.NORMAL);
+        addTasksToUser(employeeID,"newTask3",Priority.BLOCKER);
+        addTasksToUser(employeeID,"newTask4",Priority.BLOCKER);
+        addTasksToUser(employeeID,"newTask5",Priority.HIGH_PRIORITY);
+    }
+
+    public static void addTasksToUser(int userID, String name, Priority priority){
+        Employees emp = manager.find(Employees.class,userID);
+        Tasks tasks = new Tasks();
+        tasks.setName(name);
+        tasks.setPriority(priority);
+        emp.getTasksSet().add(tasks);
+        manager.getTransaction().begin();
+        manager.persist(tasks);
+        manager.persist(emp);
+        manager.getTransaction().commit();
+    }
 
     public static void extractBlockerAndChangeName(){
         manager.getTransaction().begin();
         List<Tasks> blockers = manager.createQuery(
-                "select t from Tasks t where t.priority = :priority")
+                "select t from Tasks t where t.priority = :priority",Tasks.class)
                 .setParameter("priority", Priority.BLOCKER)
                 .getResultList();
         for (Tasks t: blockers
