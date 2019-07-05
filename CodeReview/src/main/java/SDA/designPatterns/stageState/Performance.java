4819867a802900f3dc2c2280ba85a0540a673bae
Rafal Woronowicz <woronowicz87@gmail.com>
2019-07-05 16:21:47 +0200

    test

--- CodeReview/src/main/java/SDA/designPatterns/stageState/Performance.java
+++ CodeReview/src/main/java/SDA/designPatterns/stageState/Performance.java
@@ -1,4 +1,34 @@
 package SDA.designPatterns.stageState;
 
 public class Performance {
+    public static void main(String[] args) {
+        Stage stage = new Stage();
+        stage.play();
+        stage.change();
+        stage.play();
+    }
+}
+
+class Actor{
+    public void act(){}
+}
+
+class HappyActor extends Actor{
+    public void act(){
+        System.out.println("be happy");
+    }
+}
+class SadActor extends Actor{
+    public void act(){
+        System.out.println("be sad");
+    }
+}
+class Stage{
+    Actor a = new HappyActor();
+    public void change(){
+        a = new SadActor();
+    }
+    public void play(){
+        a.act();
+    }
 }
