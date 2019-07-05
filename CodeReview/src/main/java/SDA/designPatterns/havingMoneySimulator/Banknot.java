c7b009618537bfc5b040aabee2c83378983ce209
Rafal Woronowicz <woronowicz87@gmail.com>
2019-07-05 11:43:31 +0200

    deeBoop

--- CodeReview/src/main/java/SDA/designPatterns/havingMoneySimulator/Banknot.java
+++ CodeReview/src/main/java/SDA/designPatterns/havingMoneySimulator/Banknot.java
@@ -23,23 +23,6 @@ public abstract class Banknot {
         forward(change);
     }
 
-    boolean checkAmount() {
-        return amountRequested <= 0;
-    }
-
-    void caseNotEnoughResources() {
-        System.out.println("sending: " + amount + " x " + value + " PLN");
-        change += (amountRequested - amount) * value;
-        amount = 0;
-    }
-
-    void caseEnoughResources() {
-        if (amountRequested != 0) {
-            System.out.println("sending: " + amountRequested + " x " + value + " PLN");
-            amount -= amountRequested;
-        }
-    }
-
     boolean validateRequest(int request) {
         return request < 0;
     }
@@ -49,6 +32,23 @@ public abstract class Banknot {
             next.processRequest(request);
         }
         return amount == 0;
+    }
+
+    boolean checkAmount() {
+        return amountRequested <= 0;
+    }
+
+    void caseEnoughResources() {
+        if (amountRequested != 0) {
+            System.out.println("sending: " + amountRequested + " x " + value + " PLN");
+            amount -= amountRequested;
+        }
+    }
+
+    void caseNotEnoughResources() {
+        System.out.println("sending: " + amount + " x " + value + " PLN");
+        change += (amountRequested - amount) * value;
+        amount = 0;
     }
 
     void forward(int rest) {
