fd47a3155ef5702601a0dd0110027aafd35372de
WoronowiczGIT <woronowiczJava@gmail.com>
2019-07-10 13:51:16 +0200

    multibase inheretance

---
+++ Klasy Abstrakcyjne i Interfejsy/src/com/company/wielobazoweDziedziczenie/Adventure.java
@@ -0,0 +1,29 @@
+package com.company.wielobazoweDziedziczenie;
+
+public class Adventure {
+    public static void main(String[] args) {
+        Hero h = new Hero();
+        h.fly();
+        h.fight();
+    }
+}
+
+interface CanFly{
+    void fly();
+}
+interface CanFight{
+    void fight();
+}
+
+class Character{
+    public void fight(){
+        System.out.println("karate Chop");
+    }
+}
+
+class Hero extends Character implements CanFight, CanFly{
+    @Override
+    public void fly() {
+        System.out.println("fly high");
+    }
+}
