019e8631ba6b2efe8595d45f1b4cf4fed2efe3d1
WoronowiczGIT <woronowiczJava@gmail.com>
2019-07-14 10:24:28 +0200

    atak xss

---
+++ Servlety2/src/main/java/Servlet.java
@@ -0,0 +1,35 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "SuperServlet", value = "/start")
+public class Servlet extends HttpServlet {
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+
+    @Override
+    public void doPost(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws IOException {
+        this.request = servletRequest;
+        this.response = servletResponse;
+        response.setContentType("text/html");
+
+        String[] fetchedValues = request.getParameterValues("parametr1");
+        PrintWriter writer = response.getWriter();
+
+        writer.println(fetchedValues[0]);
+    }
+    @Override
+    public void doGet(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws IOException {
+        this.request = servletRequest;
+        this.response = servletResponse;
+        response.setContentType("text/html");
+
+        PrintWriter writer = response.getWriter();
+
+        writer.println("it works!");
+    }
+
+}
