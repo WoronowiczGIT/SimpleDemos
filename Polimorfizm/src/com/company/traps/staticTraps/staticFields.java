8bf05824229b42c51d741dd43bdd4a53db8777f0
Rafal Woronowicz <woronowicz87@gmail.com>
2019-07-15 19:29:21 +0200

    tried to delete workspace.xml

--- Polimorfizm/src/com/company/traps/staticTraps/staticFields.java
+++ Polimorfizm/src/com/company/traps/staticTraps/staticFields.java
@@ -1,4 +1,30 @@
 package com.company.traps.staticTraps;
 
 public class staticFields {
+
+    public static void main(String[] args) {
+        SuperClass s = new SubClass();
+        System.out.println("upcasting sub -> super");
+        System.out.println("field: "+s.value +" method: "+ s.getValue());
+
+        System.out.println("no casting");
+        SubClass s2 = new SubClass();
+        System.out.println("field: "+s2.value +" method: "+ s2.getValue()+ " super: "+s2.getSuperValue());
+        }
+}
+
+/**
+ * Fields are not polimorphic, in case of double names, we call drirectly the fields of super class,
+ * although methods are polimorphic, so they call the field dependant of the method.
+ */
+
+class SuperClass{
+    public int value = 0;
+    public int getValue (){return value;}
+}
+
+class SubClass extends SuperClass{
+    public int value = 1;
+    public int getValue() {return value;}
+    public int getSuperValue(){return super.value;}
 }
--- Polimorfizm/src/com/company/traps/staticTraps/staticMethods.java
+++ Polimorfizm/src/com/company/traps/staticTraps/staticMethods.java
@@ -1,4 +1,30 @@
 package com.company.traps.staticTraps;
 
+/**
+ * static methods depend on class not on object
+ */
+
 public class staticMethods {
+    public static void main(String[] args) {
+        SuperStaticClass s = new ChildStaticClass();
+        s.dynamicGet();
+        s.staticGet();
+    }
+}
+
+class SuperStaticClass{
+    public static void staticGet(){
+        System.out.println("call static super method");
+    }
+    public void dynamicGet(){
+        System.out.println("call dynamic super method");
+    }
+}
+class ChildStaticClass extends SuperStaticClass{
+    public static void staticGet(){
+        System.out.println("call static sub method");
+    }
+    public void dynamicGet(){
+        System.out.println("call dynamic sub method");
+    }
 }
